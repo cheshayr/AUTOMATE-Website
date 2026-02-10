c032e63e4a447b4f47c73ec775d2b9a111410c9d
jamesmlndz <james.melendez04@gmail.com>
2026-02-10 14:52:38 +0800

    Update sidebar - reports

--- src/components/app-sidebar.jsx
+++ src/components/app-sidebar.jsx
@@ -74,7 +74,7 @@ const data = {
     { url: '/user', icon: Users, name: 'User Management' },
     { url: '/feedbacks', icon: MessageCircle, name: 'Feedbacks' },
     { url: '/analytics', icon: BarChart2, name: 'Analytics' },
-    { url: '/reports', icon: FileText, name: 'Reports' },
+    { url: '/reports', icon: FileText, name: 'Performance Report' },
   ],
 
   staff: [
@@ -85,7 +85,7 @@ const data = {
     { url: '/feedbacks', icon: MessageCircle, name: 'Feedbacks' },
     //{ url: '/activities', icon: Frame, name: 'Activity Logs' },
     { url: '/analytics', icon: BarChart2, name: 'Analytics' },
-    { url: '/reports', icon: FileText, name: 'Reports' },
+    { url: '/reports', icon: FileText, name: 'Performance Report' },
 
     // { to: '/dashboard', icon: <Home size={24} />, label: 'Dashboard' },
     //   { to: '/appointments', icon: <Calendar size={24} />, label: 'Appointments' },
