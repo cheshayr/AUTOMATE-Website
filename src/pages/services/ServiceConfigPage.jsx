1ab9369f429fc77b4cd93664f20116b430cdc83a
Ches <116324470+cheshayr@users.noreply.github.com>
2026-02-13 00:59:59 +0800

    Brought back the codes (2/10/26)
    Before "PriceStartsAt" changes

--- src/pages/services/ServiceConfigPage.jsx
+++ src/pages/services/ServiceConfigPage.jsx
@@ -131,3 +131,4 @@ const ServiceConfigPage = () => {
 
 export default ServiceConfigPage;
 
+
--- src/pages/services/components/AddServiceModal.jsx
+++ src/pages/services/components/AddServiceModal.jsx
@@ -180,3 +180,5 @@ export function AddServiceModal({ isOpen, setIsOpen, currentData }) {
 export default AddServiceModal;
 
 
+
+
